8ac929984267609093f7282f12d4a75778b4a620
Jinnie <hjk7639@gmail.com>
2024-10-06 19:54:23 +0900

    Add: Extracting Loader

--- src/api/queries/searchPacakages.ts
+++ src/api/queries/searchPacakages.ts
@@ -2,7 +2,7 @@ import type { PackageSummary } from '../types/packageSummary';
 
 interface SearchResponse {
   objects: {
-    packages: {
+    package: {
       name: string;
       description: string;
       version: string;
@@ -11,11 +11,11 @@ interface SearchResponse {
   }[];
 }
 
-export async function SearchPackages(term: string): Promise<PackageSummary[]> {
+export async function searchPackages(term: string): Promise<PackageSummary[]> {
   const res = await fetch(`https://registry.npmjs.org/-/v1/search?text=${term}`);
 
   const data: SearchResponse = await res.json();
-  return data.objects.map(({ packages: { name, description, version, keywords } }) => {
+  return data.objects.map(({ package: { name, description, version, keywords } }) => {
     return {
       name,
       description,
